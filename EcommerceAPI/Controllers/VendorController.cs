fad5c10d5d8e6490bffeefe10b10a002db413088
P krishna Kumar <kkrishna37768@gmail.com>
2026-04-13 01:38:52 +0530

    Initial commit

---
+++ EcommerceAPI/Controllers/VendorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceAPI.Controllers
+{
+    public class VendorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
