fd55e104d464b8941e92aed40abdd8a1321037c9
P krishna Kumar <kkrishna37768@gmail.com>
2026-04-26 23:32:53 +0530

    .

--- EcommerceAPI/Services/DbHelper.cs
+++ EcommerceAPI/Services/DbHelper.cs
@@ -4,10 +4,7 @@ public class DbHelper
 {
     private readonly string _connectionString;
 
-    public DbHelper(IConfiguration config)
-    {
-        _connectionString = config.GetConnectionString("DefaultConnection");
-    }
+    public DbHelper(IConfiguration config) => _connectionString = config.GetConnectionString("DefaultConnection");
 
     public SqlConnection GetConnection()
     {
