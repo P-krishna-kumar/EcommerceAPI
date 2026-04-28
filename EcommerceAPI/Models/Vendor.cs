946e3e55e95ab90529bcb32f2308803a47c9eb06
P krishna Kumar <kkrishna37768@gmail.com>
2026-04-28 23:43:01 +0530

    .

--- EcommerceAPI/Models/Vendor.cs
+++ EcommerceAPI/Models/Vendor.cs
@@ -4,8 +4,8 @@
     {
         internal int Id;
         public int VendorId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
     }
 }
